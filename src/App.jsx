4eb1c28e122fa555d074db2cb6fdb3d2945a972d
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-03-30 01:22:31 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-success">Success</button>
+    </div>
+  )
+}
+
+export default App;
